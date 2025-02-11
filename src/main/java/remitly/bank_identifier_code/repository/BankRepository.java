1678a5d24890690e3180383118a4c5f1da15fff3
piotr102k <piotrswiszcz2002@wp.pl>
2025-02-11 16:10:04 +0100

    Updated BankRepository

--- src/main/java/remitly/bank_identifier_code/repository/BankRepository.java
+++ src/main/java/remitly/bank_identifier_code/repository/BankRepository.java
@@ -1,8 +1,13 @@
 package remitly.bank_identifier_code.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import remitly.bank_identifier_code.dto.BankCountryDTO;
 import remitly.bank_identifier_code.entity.Bank;
 
+import java.util.List;
 
-public interface BankRepository extends JpaRepository<Bank,Long> {
+
+public interface BankRepository extends JpaRepository<Bank,String> {
+    List<Bank> findAllBySwiftCodeStartingWith(String code);
+    List<Bank> findAllByCountryISO2(String countryISO2);
 }
