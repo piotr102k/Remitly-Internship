25ddf98461a1cb10aeaab09ee4e973c1718f4720
piotr102k <piotrswiszcz2002@wp.pl>
2025-02-11 12:55:57 +0100

    Created DTO's for bank entity

---
+++ src/main/java/remitly/bank_identifier_code/dto/BankBranchDTO.java
@@ -0,0 +1,20 @@
+package remitly.bank_identifier_code.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BankBranchDTO {
+    private String address;
+    private String bankName;
+    private String countryISO2;
+    private String countryName;
+    private boolean isHeadquarter;
+    private String swiftCode;
+
+}
