10eaa34a27edfb83f1f56363f37d64441ca036ac
piotr102k <piotrswiszcz2002@wp.pl>
2025-02-11 11:03:48 +0100

    Initial commit

---
+++ src/test/java/remitly/bank_identifier_code/BankIdentifierCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package remitly.bank_identifier_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankIdentifierCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
